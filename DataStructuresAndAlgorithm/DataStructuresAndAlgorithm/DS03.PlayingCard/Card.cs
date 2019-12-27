290c70e57c72254bb8f1db28448d525bded6e080
MrQuinPrime <52728062+MrQuinPrime@users.noreply.github.com>
2019-12-27 11:04:26 +0800

    2019/12/27

---
+++ DataStructuresAndAlgorithm/DataStructuresAndAlgorithm/DS03.PlayingCard/Card.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DS03.PlayingCard
+{
+    public class Card:IEquatable<Card>
+    {
+        public Card()
+        {
+
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithm/DataStructuresAndAlgorithm/DS03.PlayingCard/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DS03.PlayingCard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cardList = new CardCollection();
+        }
+    }
+}
