b83b56170ceae0d674084641b2774d9a2eefff25
MrQuinPrime <52728062+MrQuinPrime@users.noreply.github.com>
2020-01-10 11:27:45 +0800

    2020/1/10

---
+++ DataStructuresAndAlgorithm/DataStructuresAndAlgorithm/DS06.BinarySTree/BinaryTreeNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DS06.BinarySTree
+{
+    public class BinaryTreeNode
+    {
+        public string Name;              // 节点名称
+        public double Value;             // 节点对象
+        public BinaryTreeNode LeftNode;  // 左节点 
+        public BinaryTreeNode RightNode; // 右节点
+
+        public BinaryTreeNode(string name, double value)
+        {
+            this.Name = name;
+            this.Value = value;
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithm/DataStructuresAndAlgorithm/DS06.BinarySTree/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DS06.BinarySTree
+{
+    class Program
+    {
+        static void Main()
+        {
+            var bt = new BinaryTree();
+            bt.Insert("D", 10000);
+            bt.Insert("B", 10000);
+            bt.Insert("F", 10000);
+            bt.Insert("A", 10000);
+            bt.Insert("C", 10000);
+            bt.Insert("E", 10000);
+            bt.Insert("G", 10000);
+        }
+    }
+}
